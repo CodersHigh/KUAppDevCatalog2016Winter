bfdf36d075e4f333ee9ed0cc4517c707b25ffafe
skyshow2 <kihyun-soyeon@Kihyun-Soyeonui-MacBook-Air.local>
2016-01-27 13:31:34 +0900

    KihyunBaik playground

---
+++ kihyunBaik/Playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var greeting = "Hello,"
+
+print("32+64 = \(32+64)")
+
+var names=["kihyunbaik","ku", "skdjak"]
+let myName="kihyun"
+
+for name in names{
+    print("\(greeting)!\(name)from \(myName)")}
+
+
+let maxSpeed:Int = 110
+
+var currentSpeed = 100
+currentSpeed+=10
+//currentSpeed=105.7
+
+var gpsSpeed:Double = 105.7
+
+var speedHistory:[Int] = []
+speedHistory += [currentSpeed]
+let gpsSpeed0901=114.1
+speedHistory.append(Int(gpsSpeed0901))
+speedHistory[0]
+speedHistory.last
+
+let experimenter:String="실험자"
+let subject:String="피험자"
+
+print; subject
