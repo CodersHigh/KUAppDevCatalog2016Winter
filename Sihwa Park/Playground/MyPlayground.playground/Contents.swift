f6d80cd2cd79dc884452269e7fcb5b54b91340cc
astrayne <cscoi011@cscoi011ui-iMac.local>
2016-01-27 14:45:58 +0900

    commit

--- Sihwa Park/Playground/MyPlayground.playground/Contents.swift
+++ Sihwa Park/Playground/MyPlayground.playground/Contents.swift
@@ -58,10 +58,41 @@ let roomNames = [String](roomCapacity.keys)
 
 
 var fake_data = [["대한민국, 서울","인천 국제 공항"], ["프랑스, 파리", "샤를 드골 공항", "에펠 탑", "에투알 개선문", "샹젤리제", "루브르 박물관", "노트르담 대성당"], ["영국, 런던", "히드로 공항", "대영박물관", "버킹엄 궁전"], ["미국, 뉴욕", "뉴왁국제공항", "뉴욕 타임스 스퀘어", "뉴욕 센트럴파크"]]
+var seoulpoi:[String:[String]] = ["대한민국, 서울":["인천 국제 공항"]]
+var parispoi = ["프랑스, 파리":["샤를 드골 공항", "에펠 탑", "에투알 개선문", "샹젤리제", "루브르 박물관", "노트르담 대성당"]]
+var londonpoi = ["영국, 런던":["히드로 공항", "대영박물관", "버킹엄 궁전"]]
+var newyorkpoi = ["미국, 뉴욕":["뉴왁국제공항", "뉴욕 타임스 스퀘어", "뉴욕 센트럴파크"]]
+
+var worldpoi = [seoulpoi, parispoi, londonpoi, newyorkpoi]
 
 
 fake_data[0]
 
+
+
+var year:Int = 2012
+
+if(year%4==0)
+{
+
+    if(year%400==0)
+    {
+        print("\(year)은 윤년입니다.")
+    }
+    else if(year%100==0)
+    {
+        print("\(year)은 윤년이 아닙니다.")
+    }
+    else
+    {
+        print("\(year)은 윤년입니다.")
+    }
+
+}
+else
+{
+    print("\(year)은 윤년이 아닙니다.")
+}
 
 
 
