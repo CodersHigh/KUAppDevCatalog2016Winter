14008f28b184b3fbff09d7be320207a8aca9f057
skyshow2 <kihyun-soyeon@Kihyun-Soyeonui-MacBook-Air.local>
2016-01-27 14:54:25 +0900

    Play

--- kihyunBaik/Playground/MyPlayground.playground/Contents.swift
+++ kihyunBaik/Playground/MyPlayground.playground/Contents.swift
@@ -1,34 +1,7 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var greeting = "Hello,"
-
-print("32+64 = \(32+64)")
-
-var names=["kihyunbaik","ku", "skdjak"]
-let myName="kihyun"
-
-for name in names{
-    print("\(greeting)!\(name)from \(myName)")}
-
-
-let maxSpeed:Int = 110
-
-var currentSpeed = 100
-currentSpeed+=10
-//currentSpeed=105.7
-
-var gpsSpeed:Double = 105.7
-
-var speedHistory:[Int] = []
-speedHistory += [currentSpeed]
-let gpsSpeed0901=114.1
-speedHistory.append(Int(gpsSpeed0901))
-speedHistory[0]
-speedHistory.last
-
-let experimenter:String="실험자"
-let subject:String="피험자"
-
-print; subject
+var n:Int = Unit8
+var year:Int = 2016
+if year==4*n{
+    print("\(year)은 윤년입니다.")
+} else {
+    print("\(year)은 윤년이 아닙니다.")
+}
