aff9a50a16259d09031c8db4097ac12ab1c24743
spider-pig <cscoi008@cscoi008ui-iMac.local>
2016-01-28 17:29:35 +0900

    README수정, MyPlayGround3

--- YoonsungKim/Playground/MyPlayground3.playground/Contents.swift
+++ YoonsungKim/Playground/MyPlayground3.playground/Contents.swift
@@ -24,7 +24,7 @@ func ratingRecord (history:[Double]) -> (average:Double, min:Double, max:Double)
     
     return (sum/Double(history.count), min, max)
 }
-
+0
 ratings = []
 
 var bookDescription: String = "\(title)"
@@ -36,3 +36,102 @@ if let theRatings = ratings, record = ratingRecord(theRatings) {
 }
 
 print("\(bookDescription)")
+
+var base = 2
+var power = 9
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+}
+
+for ch in "HELLO".characters{
+    print(ch)
+}
+
+for (idx,ch) in "HELLO".characters.enumerate() {
+    print("index : " + String(ch))
+}
+var ch = "0"
+
+switch ch {
+case "a":
+    "match"
+default:
+    "unmatch"
+}
+
+var strInput = "Swift is very fast"
+var strOutput = ""
+
+for ch in strInput.characters {
+    switch ch {
+        case " ": fallthrough
+    default :
+        strOutput.append(ch)
+    }
+}
+
+strOutput
+
+func greet(name:String, day:String) -> (String)
+{
+    return "Hello \(name), today is \(day)"
+}
+
+greet("Kathy", day: "Wednesday")
+
+func getPersonInfo() -> (Int, String, Int)
+{
+    return (27,"홍길동", 106)
+}
+
+getPersonInfo()
+
+func swap(p1:Int, p2:Int) -> (Int, Int)
+{
+    return (p2, p1)
+}
+
+swap(12, p2: 15)
+
+func greeting()
+{
+    print("hello")
+}
+
+func greeting(name:String)
+{
+    print("\(name) hello")
+}
+
+func greeting(no name:String)
+{
+    print("\(name) hello????")
+}
+
+
+greeting()
+greeting("홍길동")
+greeting("홍길순")
+greeting(no: "호이")
+
+func swap(n1:Int, n2:Int) -> (Int, Int)
+{
+    return (n2, n1)
+}
+
+var n1 = 10, n2 = 20
+
+(n1, n2) = swap(n1, n2: n2)
+
+n1
+n2
+
+
+
+
+
+
+
+
--- YoonsungKim/README.md
+++ YoonsungKim/README.md
@@ -12,3 +12,27 @@ let a = optionalString
 - if let 다음에 나오는 변수는 if문이 끝나면 사라진다.
 
 optional 변수에 값을 넣을 때는 optional을 unwrapping할 필요 없지만 optional 변수에 **접근, 변경하는 경우** 반드시 unwrapping해야한다.
+
+<br>
+##제어문
+###switch-case
+case 에 아무런 문장이 없으면 안된다.
+break 가 필요없다.  
+fallthrough 를 자동 지원하지 않는다.    
+continue: default 건너뛰도록 함 (switch문 끝)  
+fallthrough: default 건너뛰지 않음(switch문 계속 진행)
+
+<br>
+##Function
+함수 호출시 매개변수가 2개 이상이면 **두 번째 매개변수부터는 반드시 레이블을 붙여서 호출해야한다.(첫 번째 매개변수는 레이블을 붙이면 안됌. 붙이려면 레이블 따로 지정)**  
+ex) func greet(name:String, day:String) -> (String)  
+greet("홍길동", day: "수요일") : o  
+greet("홍길동", "수요일") : x  
+greet(name: "홍길동", day: "수요일") : x  <br><br>
+레이블 따로 지정하는 법   
+func greet(name na:String)  
+name : 레이블, na : 매개변수 명
+<br><br>
+**함수의 중복정의가 가능** ; 매개변수로 구분  
+
+ex) func greeting() != func greeting(name:String)
