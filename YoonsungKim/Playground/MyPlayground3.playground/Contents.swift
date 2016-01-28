a0f4de57d1285ffc3323e1a7971f83b91a8b13a7
spider-pig <cscoi008@cscoi008ui-iMac.local>
2016-01-28 14:46:52 +0900

    func, optional

--- YoonsungKim/Playground/MyPlayground3.playground/Contents.swift
+++ YoonsungKim/Playground/MyPlayground3.playground/Contents.swift
@@ -6,47 +6,9 @@ var title:String = "Storyboard Prototyping"
 var ratings : [Double]? = nil
 var supportURL : String? = nil
 
-ratings = [4.5]
-
-print("There are \(ratings) ratings")
-
-if let opRate = ratings {
-    print("There are \(ratings) ratings")
-} else {
-    print("ratings is nil")
-}
-
-var value = "234"
-
-func findStockCode(company : String) -> String? {
+func ratingRecord (history:[Double]) -> (average:Double, min:Double, max:Double)? {
     
-    if (company == "Apple") {
-        return "AAPL"
-    } else if (company == "Google") {
-        return "GOOG"
-    }
-    return nil
-}
-
-var stockCode:String? = findStockCode("Facebook")
-let text = "Stock Code - "
-
-if let stockCode = stockCode {
-    let message = text + stockCode
-    print("\(message)")
-}
-
-print("\(stockCode)")
-
-let optionalEmail: Int? = 20
-let requiredEmail: String? = "Hi"
-let a = optionalEmail
-
-if let v = optionalEmail where v > 10 {
-    print("\(v)")
-}
-
-func ratingRecord (history:[Double]) -> (average:Double, min:Double, max:Double) {
+    if history == [] { return nil }
     
     var sum = 0.0, min = history[0], max = history[0]
     
@@ -67,9 +29,10 @@ ratings = []
 
 var bookDescription: String = "\(title)"
 
-if let theRatings = ratings where theRatings != []{
-    let record = ratingRecord(theRatings)
-    bookDescription += "has \(theRatings.count) ratings, \r\n average is \(record.average), from \(record.min) to \(record.max)"
+if let theRatings = ratings, record = ratingRecord(theRatings) {
+    bookDescription += "has \(theRatings.count) ratings, \raverage is \(record.average), from \(record.min) to \(record.max)"
+} else {
+    print("There is no rating yet")
 }
 
 print("\(bookDescription)")
