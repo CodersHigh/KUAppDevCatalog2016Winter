e542a979339a33d5869ca4229e7d694636c924f2
skyshow2 <kihyun-soyeon@Kihyun-Soyeonui-MacBook-Air.local>
2016-02-04 14:45:44 +0900

    commit

--- kihyunBaik/Playground/MyPlayground.playground/Contents.swift
+++ kihyunBaik/Playground/MyPlayground.playground/Contents.swift
@@ -1,53 +1,21 @@
-//var n:Int = Unit8
-var year:Int = 2016
-//if year==4*n{
-    print("\(year)은 윤년입니다.")
-//} else {
-    print("\(year)은 윤년이 아닙니다.")
-//}
+class UserInformation {
+    var SeoulDistrict:Array<String> = ["강남, 서초, 송파", "강동, 광진, 성동", "도봉, 노원, 중랑", "강북, 성북, 동대문", "종로, 용산, 중구", "서대문, 마포", "은평, 강서", "동작, 관악, 영등포", "양천, 구로, 금천"]
+    var Busan:Array<String> = ["부산"]
+    var Daegu:Array<String> = ["대구"]
+    var Gwangju:Array<String> = ["광주"]
+    var Kyungki:Array<String> = ["경기"]
+    var Ulsan:Array<String> = ["울산"]
+    var Incheon:Array<String> = ["인천"]
+    var Daejeon:Array<String> = ["대전"]
+    var Jeju:Array<String> = ["제주"]
+    var Kyungbook:Array<String> = ["경북"]
+    var Kyungnam:Array<String> = ["경남"]
+    var Jeonnam:Array<String> = ["전남"]
+    var Jeonbook:Array<String> = ["전북"]
+    var Kangwon:Array<String> = ["강원"]
+    var Choongnam:Array<String> = ["충남"]
+    var Choongbook:Array<String> = ["충북"]
 
-var title : String = "Storyboard Prototyping"
-var ratings : [Int]? = nil
-var supportURL : String? = nil
-
-//print ("\(title) has \(ratings!.count) ratings. \r\nsupport
-//web page : \(supportURL)")
-
-supportURL = "www.codershigh.com"
-
-var bookDescription:String="\(title)"
-
-if ratings != nil {
-    bookDescription += "has\(ratings!.count) ratings"
+    var Age:Int?
+    var Gender:Array<String> = ["남성", "여성"]
 }
-
-if supportURL != nil {
-    bookDescription += "\r\nsupport web page: \(supportURL)"
-}
-
-//print("\(bookDescription)")
-
-//if let theRatings = ratings{
-  //  bookDescription += "has\(theRatings.count) ratings"
-//}
-//if let theURL = supportURL {
-    //bookDescription += "\r\nsupport web page: \(theURL)"
-//}
-
-
-func ratingRecord (history: [Double])->(average:Double, min:Double, max:Double){
-    if history.count == 0 { return nil }
-
-    var sum=0.0, min = history[0], max = history[0]
-    for value in history{
-        if min>value{min=value}
-        if max<value{max=value}
-        sum+=value
-    }
-
-    let average = sum / Double(history.count)
-    return (average, min, max)
-}
-
-ratings = [4.5, 3.0, 5.0, 2.5]
-bookDescription = "\(title)"
