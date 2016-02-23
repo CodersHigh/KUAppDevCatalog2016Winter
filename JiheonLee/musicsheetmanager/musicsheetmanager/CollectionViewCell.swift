8155cbb785f6b79659714344d830685a5d4c4860
JiheonLee <dannyjh32@naver.com>
2016-02-23 14:54:13 +0900

    폴더생성
    폴더생성

---
+++ JiheonLee/musicsheetmanager/musicsheetmanager/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  musicsheetmanager
+//
+//  Created by cscoi021 on 2016. 2. 18..
+//  Copyright © 2016년 cscoi021. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+   
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var lblaaa: UILabel!
+    
+    
+}
---
+++ JiheonLee/musicsheetmanager/musicsheetmanager/WebViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  musicsheetmanager
+//
+//  Created by cscoi021 on 2016. 2. 16..
+//  Copyright © 2016년 cscoi021. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet var viewScore: UIWebView!
+
+    @IBOutlet weak var met: UIBarButtonItem!
+    
+    var selectedSheet:String?
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let aaa = selectedSheet{        
+            
+            let localfilePath = NSBundle.mainBundle().URLForResource(aaa, withExtension: "pdf");
+            let myRequest = NSURLRequest(URL: localfilePath!);
+            viewScore.loadRequest(myRequest);
+        }
+        
+        met.title = "50"
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
